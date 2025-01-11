79aee2258db1e16cc6a1afca5b8775a9578e5a3f
Soru <chansolchoe9216@gmail.com>
2025-01-12 04:10:10 +0900

    [chore] Delete subtree push cmd to forked origin default

--- scripts/sync-upstream.sh
+++ scripts/sync-upstream.sh
@@ -4,23 +4,17 @@ ROOT_DIR="$(pwd)"
 
 echo "Syncing apexcharts from upstream-apexcharts..."
 
-# 1) Merge changes from the main branch of upstream-apexcharts into packages/apexcharts
+# Merge changes from the main branch of upstream-apexcharts into packages/apexcharts
 git subtree pull --prefix=packages/apexcharts upstream-apexcharts main
-
-# 2) Push the changes to the forked (origin) repository
-git subtree push --prefix=packages/apexcharts origin-apexcharts main
 
 echo "apexcharts synced."
 
 echo "Syncing react-apexcharts from upstream-react-apexcharts..."
 
-# 1) Merge changes from the main branch of upstream-react-apexcharts into packages/react-apexcharts
+# Merge changes from the main branch of upstream-react-apexcharts into packages/react-apexcharts
 git subtree pull --prefix=packages/react-apexcharts upstream-react-apexcharts master
 
-# 2) Push the changes to the forked (origin) repository
-git subtree push --prefix=packages/react-apexcharts origin-react-apexcharts master
-
-# 3) Modify package.json
+# Modify package.json
 jq '.peerDependencies.apexcharts = "workspace:packages/apexcharts"' \
   packages/react-apexcharts/package.json > tmp.json && mv tmp.json packages/react-apexcharts/package.json
 
