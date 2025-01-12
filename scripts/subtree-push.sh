f8f23f927df1e30122f17fb283d33c58ee6692cc
Soru <chansolchoe9216@gmail.com>
2025-01-12 10:40:20 +0900

    [chore] Split subtree comit

--- scripts/subtree-push.sh
+++ scripts/subtree-push.sh
@@ -9,10 +9,17 @@ if [ -z "$current_branch" ]; then
   exit 1
 fi
 
-# Check if the branch exists in origin-apexcharts and perform subtree push
+# Check if the branch exists in origin-apexcharts and perform subtree split and push
 if git ls-remote --exit-code --heads origin-apexcharts "$current_branch" > /dev/null; then
-  echo "Subtree push: packages/apexcharts -> origin-apexcharts/$current_branch"
-  git subtree push --prefix=packages/apexcharts origin-apexcharts "$current_branch"
+  echo "Subtree split: packages/apexcharts -> origin-apexcharts/$current_branch"
+
+  apexcharts_commit=$(git subtree split --prefix=packages/apexcharts HEAD)
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to split subtree for packages/apexcharts."
+    exit 1
+  fi
+
+  git push origin-apexcharts "$apexcharts_commit:$current_branch" --force
   if [ $? -ne 0 ]; then
     echo "Error: Subtree push for packages/apexcharts failed."
     exit 1
@@ -22,10 +29,17 @@ else
   exit 1
 fi
 
-# Check if the branch exists in origin-react-apexcharts and perform subtree push
+# Check if the branch exists in origin-react-apexcharts and perform subtree split and push
 if git ls-remote --exit-code --heads origin-react-apexcharts "$current_branch" > /dev/null; then
-  echo "Subtree push: packages/react-apexcharts -> origin-react-apexcharts/$current_branch"
-  git subtree push --prefix=packages/react-apexcharts origin-react-apexcharts "$current_branch"
+  echo "Subtree split: packages/react-apexcharts -> origin-react-apexcharts/$current_branch"
+
+  react_apexcharts_commit=$(git subtree split --prefix=packages/react-apexcharts HEAD)
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to split subtree for packages/react-apexcharts."
+    exit 1
+  fi
+
+  git push origin-react-apexcharts "$react_apexcharts_commit:$current_branch" --force
   if [ $? -ne 0 ]; then
     echo "Error: Subtree push for packages/react-apexcharts failed."
     exit 1
@@ -35,4 +49,4 @@ else
   exit 1
 fi
 
-echo "Subtree push completed successfully."
+echo "Subtree split and push completed successfully."
