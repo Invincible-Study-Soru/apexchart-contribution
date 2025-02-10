b59a3c3e1efbc00d77bee2d6060891e35d197cb6
hhjeee <yoonhj0315@gmail.com>
2025-02-10 20:27:56 +0900

    [chore] donut thickness test code

---
+++ apps/react-test/src/chart/chart3.tsx
@@ -0,0 +1,41 @@
+import Chart from "react-apexcharts";
+
+const Chart3 = () => {
+  return (
+    <Chart
+        width="100%"
+        height="100%"
+        type="donut"
+        options={{
+            chart: {
+                toolbar: {
+                    show: false
+                },
+                zoom: {
+                    enabled: false
+                },
+            },
+            dataLabels: {
+                enabled: true,
+            },
+            tooltip: {
+                fixed: {
+                    enabled: true,
+                },
+            },
+            plotOptions: {
+                pie:{
+                    expandOnClick: true,
+                    donut:{
+                        size: '30%',
+                        //thickness: [0.5, 1, 0.7, 0.8, 0.2],
+                    }
+                }
+            },
+        }}
+        series={[44, 55, 41, 17, 15]}
+  />
+  );
+};
+
+export default Chart3;
